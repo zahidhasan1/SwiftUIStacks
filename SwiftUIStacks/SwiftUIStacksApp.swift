94f9d4d1c625fdf9fb0a17485a05dacd299d547b
Zahid Hasan <megamind.d.evilll@gmail.com>
2023-11-08 18:46:30 +0600

    Initial Commit

---
+++ SwiftUIStacks/SwiftUIStacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStacksApp.swift
+//  SwiftUIStacks
+//
+//  Created by ZEUS on 8/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
