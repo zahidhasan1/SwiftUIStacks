c4628f9bb0fae86659a3cd1f771f60753bb26bc4
Zahid Hasan <megamind.d.evilll@gmail.com>
2023-11-08 20:16:30 +0600

    Added Zstack

--- SwiftUIStacks/ContentView.swift
+++ SwiftUIStacks/ContentView.swift
@@ -11,13 +11,21 @@ struct ContentView: View {
     var body: some View {
         VStack {
             HeaderView()
-            
-            HStack {
-                PricingView(title: "Basic", price: "$9", textColor: .white, bgColor: .purple)
-                PricingView(title: "Pro", price: "$19", textColor: .black, bgColor: Color(red: 240/255, green: 240/255, blue: 240/255))
+            VStack(spacing: 20){
+                HStack(spacing: 20){
+                    PricingView(title: "Basic", price: "$9", textColor: .white, bgColor: .purple)
+                    ZStack {
+                        PricingView(title: "Pro", price: "$19", textColor: .black, bgColor: Color(red: 240/255, green: 240/255, blue: 240/255))
+                        BadgeLabelView(badgeText: "Best For Designer", offsetY: 87)
+                    }
+                }
+                
+                ZStack {
+                    TeamPricingView()
+                    BadgeLabelView(badgeText: "Perfect for teams with 20 members", offsetY: 125)
+                }
             }
-            .padding(.horizontal)
-        }
+        }.padding(20)
     }
 }
 
@@ -61,5 +69,42 @@ struct PricingView: View {
         .padding(40)
         .background(bgColor)
         .cornerRadius(10)
+    }
+}
+
+struct TeamPricingView: View {
+    var body: some View {
+        VStack{
+            Image(systemName: "wand.and.rays")
+                .font(.system(size: 40))
+                .foregroundColor(.white)
+            Text("Team")
+                .font(.system(size: 40, weight: .bold, design: .rounded))
+                .foregroundColor(.white)
+            Text("$299")
+                .font(.system(size: 50, weight: .heavy, design: .rounded))
+                .foregroundColor(.white)
+            Text("per month")
+                .font(.headline)
+                .foregroundColor(.white)
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
+        .padding(40)
+        .background(Color(red: 64/255, green: 64/255, blue: 72/255))
+        .cornerRadius(10)
+    }
+}
+
+struct BadgeLabelView: View {
+    var badgeText: String
+    var offsetX: CGFloat = 0.0
+    var offsetY: CGFloat = 0.0
+    var body: some View {
+        Text(badgeText)
+            .font(.system(.caption, design: .rounded, weight: .bold))
+            .foregroundColor(.white)
+            .padding(5)
+            .background(Color(red: 255/255, green: 183/255, blue: 37/255))
+            .offset(x:offsetX, y: offsetY)
     }
 }
